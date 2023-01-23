d8efa3061dfd932265ea3916997505911db72abd
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-01-23 11:23:57 +0100

    Initial commit

---
+++ src/test/java/com/example/milkappbackend/MilkAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.milkappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
